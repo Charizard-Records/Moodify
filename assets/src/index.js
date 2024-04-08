4e079b9bbe2fb072189c1df6a0905ba73134b9ea
d.wong92 <d.wong92@gmail.com>
2024-04-08 18:00:20 -0400

    save

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,3 +1,9 @@
-import './main.css';
-import {test} from './test';
-test();
+// import './main.css';
+// import {test} from './test';
+// import App from "./App.jsx";
+
+import { createRoot } from "react-dom/client";
+import App from "./app";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
