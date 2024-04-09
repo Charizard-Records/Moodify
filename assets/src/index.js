68d18b2b480624c38389443b9cbce038e1854d99
d.wong92 <d.wong92@gmail.com>
2024-04-09 12:11:16 -0400

    Finalizing Webpack, React-Router

--- assets/src/index.js
+++
@@ -1,9 +0,0 @@
-// import './main.css';
-// import {test} from './test';
-// import App from "./App.jsx";
-
-import { createRoot } from "react-dom/client";
-import App from "./app";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
